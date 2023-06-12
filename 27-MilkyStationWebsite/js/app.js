60dd5ab9ab1fa17ffdc5abf2396efa1fd916e218
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-06-12 17:41:44 +0800

    add show navigation - javascript and css

--- 27-MilkyStationWebsite/js/app.js
+++ 27-MilkyStationWebsite/js/app.js
@@ -3,5 +3,15 @@
 const date = (document.getElementById("date").innerHTML =
   new Date().getFullYear());
 
-// // setup nav
-// const nabBtn = document.querySelector(".nav-btn");
+// setup nav
+const navBtn = document.getElementById("nav-btn");
+const navbar = document.getElementById("navbar");
+const navClose = document.getElementById("nav-close");
+
+navBtn.addEventListener("click", () => {
+  navbar.classList.add("showNav");
+});
+
+navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
